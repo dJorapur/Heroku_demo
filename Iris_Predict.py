2a426dd13f4d30c06c94221128738e8cb84a34c9
dJorapur <45754016+dJorapur@users.noreply.github.com>
2021-11-17 04:31:40 -0800

    Add files via upload

---
+++ Iris_Predict.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import joblib
+import numpy as np
+from flask import Flask,request
+
+app = Flask(__name__)
+
+model = joblib.load(open('Iris_Model.pkl','rb'))
+
+@app.route('/predict', methods=['GET','POST'])
+def predict():
+    if request.method == 'GET':
+        return 'Please send POST request'
+    elif request.method == "POST":
+        data = request.get_json()
+        sp = data['sp']
+        sw = data['sw']
+        pl = data['pl']
+        pw = data['pw']
+        
+        in1= np.array([[sp,sw,pl,pw]])
+        flower = model.predict(in1)
+        print(flower)
+        return str(flower)
+    
+if __name__ == "main":
+    app.run()
