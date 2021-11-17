3194bad92c0e0d88bbd542145be3eb231b91ff73
dJorapur <45754016+dJorapur@users.noreply.github.com>
2021-11-17 04:38:43 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import joblib
+import numpy as np
+from flask import Flask,request
+
+app = Flask(__name__)
+
+model = joblib.load(open('Iris_Model.pkl','rb'))
+
+@app.route('/predict', methods=['GET','POST'])
+def predict():
+    if request.method == 'GET':
+        return 'Please send POST request'
+    elif request.method == "POST":
+        data = request.get_json()
+        sp = data['sp']
+        sw = data['sw']
+        pl = data['pl']
+        pw = data['pw']
+        
+        in1= np.array([[sp,sw,pl,pw]])
+        flower = model.predict(in1)
+        print(flower)
+        return str(flower)
+    
+if __name__ == "main":
+    app.run()
